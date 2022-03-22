91430d9364c0307f487e4780a9ba35e1f276243a
KnightYan <yan_cristian_s@hotmail.com>
2022-03-22 15:19:32 -0300

    Adicionar arquivos de projeto.

---
+++ WinTheme/FrmPrincipal.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WinTheme
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlightColor = new System.Windows.Forms.Button();
+            this.btndarkColor = new System.Windows.Forms.Button();
+            this.btnthemeColor = new System.Windows.Forms.Button();
+            this.pnllightColor = new System.Windows.Forms.Panel();
+            this.pnldarkColor = new System.Windows.Forms.Panel();
+            this.pnlthemeColor = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnlightColor
+            // 
+            this.btnlightColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlightColor.Location = new System.Drawing.Point(103, 178);
+            this.btnlightColor.Name = "btnlightColor";
+            this.btnlightColor.Size = new System.Drawing.Size(110, 95);
+            this.btnlightColor.TabIndex = 0;
+            this.btnlightColor.Text = "lightColor";
+            this.btnlightColor.UseVisualStyleBackColor = true;
+            // 
+            // btndarkColor
+            // 
+            this.btndarkColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndarkColor.Location = new System.Drawing.Point(345, 178);
+            this.btndarkColor.Name = "btndarkColor";
+            this.btndarkColor.Size = new System.Drawing.Size(110, 95);
+            this.btndarkColor.TabIndex = 1;
+            this.btndarkColor.Text = "darkColor";
+            this.btndarkColor.UseVisualStyleBackColor = true;
+            // 
+            // btnthemeColor
+            // 
+            this.btnthemeColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnthemeColor.Location = new System.Drawing.Point(567, 178);
+            this.btnthemeColor.Name = "btnthemeColor";
+            this.btnthemeColor.Size = new System.Drawing.Size(110, 95);
+            this.btnthemeColor.TabIndex = 2;
+            this.btnthemeColor.Tag = "winTheme";
+            this.btnthemeColor.Text = "themeColor";
+            this.btnthemeColor.UseVisualStyleBackColor = true;
+            // 
+            // pnllightColor
+            // 
+            this.pnllightColor.Location = new System.Drawing.Point(71, 46);
+            this.pnllightColor.Name = "pnllightColor";
+            this.pnllightColor.Size = new System.Drawing.Size(162, 99);
+            this.pnllightColor.TabIndex = 3;
+            // 
+            // pnldarkColor
+            // 
+            this.pnldarkColor.Location = new System.Drawing.Point(321, 46);
+            this.pnldarkColor.Name = "pnldarkColor";
+            this.pnldarkColor.Size = new System.Drawing.Size(162, 99);
+            this.pnldarkColor.TabIndex = 4;
+            // 
+            // pnlthemeColor
+            // 
+            this.pnlthemeColor.Location = new System.Drawing.Point(541, 46);
+            this.pnlthemeColor.Name = "pnlthemeColor";
+            this.pnlthemeColor.Size = new System.Drawing.Size(162, 99);
+            this.pnlthemeColor.TabIndex = 4;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlthemeColor);
+            this.Controls.Add(this.pnldarkColor);
+            this.Controls.Add(this.pnllightColor);
+            this.Controls.Add(this.btnthemeColor);
+            this.Controls.Add(this.btndarkColor);
+            this.Controls.Add(this.btnlightColor);
+            this.Name = "FrmPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlightColor;
+        private System.Windows.Forms.Button btndarkColor;
+        private System.Windows.Forms.Button btnthemeColor;
+        private System.Windows.Forms.Panel pnllightColor;
+        private System.Windows.Forms.Panel pnldarkColor;
+        private System.Windows.Forms.Panel pnlthemeColor;
+    }
+}
---
+++ WinTheme/FrmPrincipal.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WinTheme
+{
+    public partial class FrmPrincipal : Form
+    {
+        private UserPreferenceChangedEventHandler UserPreferenceChanged;
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            LoadTheme();
+            UserPreferenceChanged = new UserPreferenceChangedEventHandler(SystemEvents_UserPreferenceChanged);
+            SystemEvents.UserPreferenceChanged += UserPreferenceChanged;
+            this.Disposed += new EventHandler(Form_Disposed);
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadTheme()
+        {
+            var themeColor = WinTheme.GetAccentColor();//Windows Accent Color
+            var lightColor = ControlPaint.Light(themeColor);
+            var darkColor = ControlPaint.Dark(themeColor);
+            /*btnlightColor.BackColor = themeColor;
+            btndarkColor.BackColor = darkColor;
+            btnthemeColor.BackColor = themeColor;
+
+            pnllightColor.BackColor = themeColor;
+            pnldarkColor.BackColor = darkColor;
+            pnlthemeColor.BackColor = lightColor;*/
+
+            //var TitleBar = 
+
+
+            //Buttons
+            foreach (Button button in this.Controls.OfType<Button>())
+            {
+                if ("winTheme" == (string)button.Tag)
+                {
+                    button.BackColor = lightColor;
+                    button.ForeColor = darkColor;
+                }
+            }
+
+            foreach (Panel panel in this.Controls.OfType<Panel>())
+            {
+                panel.BackColor = themeColor;
+            }
+        }
+
+        private void SystemEvents_UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            if (e.Category == UserPreferenceCategory.General || e.Category == UserPreferenceCategory.VisualStyle)
+            {
+                LoadTheme();
+            }
+        }
+        private void Form_Disposed(object sender, EventArgs e)
+        {
+            SystemEvents.UserPreferenceChanged -= UserPreferenceChanged;
+        }
+
+        /*[DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("User32.dll")]
+        private static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            const int WM_NCPAINT = 0x85;
+            if (m.Msg == WM_NCPAINT)
+            {
+                IntPtr hdc = GetWindowDC(m.HWnd);
+                if ((int)hdc != 0)
+                {
+                    Graphics g = Graphics.FromHdc(hdc);
+                    g.FillRectangle(Brushes.Green, new Rectangle(0, 0, 4800, 23));
+                    g.Flush();
+                    ReleaseDC(m.HWnd, hdc);
+                }
+            }
+        }*/
+    }
+}
